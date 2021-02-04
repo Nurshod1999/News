d2bb73b037b18499f26543fb80ab9ccaf7724c83
Nodirbek <undefined.6142@gmail.com>
2021-02-04 20:16:17 +0500

    favourite

--- src/screens/FavouritesTab.jsx
+++ src/screens/FavouritesTab.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { ScrollView } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+import { useIsFocused } from '@react-navigation/native'
+import { useTabBarHeader } from '../hooks/helpers'
+import FavouriteItem from '../components/FavouriteItem'
 
-export default function FavouritesTab() {
+export default function FavouritesTab(props) {
+    const [favourites, setFavourites] = useState([])
+    const isFocused = useIsFocused()
+    useTabBarHeader({
+        title: 'Sevimlilar',
+    })
+
+    useEffect(() => {
+        loadFavourites()
+    }, [isFocused, props])
+
+    async function loadFavourites() {
+        const items = await AsyncStorage.getItem('favourites')
+        setFavourites(JSON.parse(items || '[]'))
+    }
+
     return (
-        <View><Text>Favourites Tab</Text></View>
+        <ScrollView style={{ paddingTop: 10 }}>
+            {favourites.map((item) => (
+                <FavouriteItem key={item.id} item={item} />
+            ))}
+        </ScrollView>
     )
 }
