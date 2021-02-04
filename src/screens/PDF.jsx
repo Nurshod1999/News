80992931faf87e89ec5b44f226a708449034a320
Nodirbek <undefined.6142@gmail.com>
2021-02-04 22:13:40 +0500

    fixes

--- src/screens/PDF.jsx
+++ src/screens/PDF.jsx
@@ -13,17 +13,3 @@ export default function WorkDetail({ route }) {
         />
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        marginTop: 25,
-    },
-    pdf: {
-        flex: 1,
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-    },
-})
